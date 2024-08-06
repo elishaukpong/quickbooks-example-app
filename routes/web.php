3653478c5baa95e6893f7178475031116027ef9d
Elisha Ukpong <ishukpong418@gmail.com>
2024-08-06 11:25:12 +0100

    installed breeeze

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Http\Controllers\BuyerController;
-use App\Http\Controllers\QuickBooksController;
-use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('buyer', [BuyerController::class,'index']);
-Route::get('supplier', [SupplierController::class,'index']);
-Route::get('quickbooks/connect', [QuickBooksController::class,'index']);
-Route::get('quickbooks/connect/auth', [QuickBooksController::class,'connect'])->name('quickbooks.auth');
-Route::get('quickbooks/connect/callbacck', [QuickBooksController::class,'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
